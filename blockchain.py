f9cb2ea4bbb868f76da9b39c99856ddb61b4e6ba
wallsmanBR <alves_thiagobastos@hotmail.com>
2018-02-02 13:10:08 -0200

    primeiro commit com o esqueleto do blockchain

---
+++ blockchain.py
@@ -0,0 +1,19 @@
+class Blockchain(object):
+    def __init__(self):
+        self.chain = []
+        self.current_transaction = []
+
+    def new_block(self):
+        pass
+
+    def new_transaction(self):
+        pass
+
+    @staticmethod
+    def hash(block):
+        pass
+
+    @property
+    def last_block(self):
+        pass
+    
